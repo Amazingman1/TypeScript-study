1b1022b330e8553c6dda51a89bfa326d9d86bb7b
wangxaun <xuan.wang@vonechain.com>
2021-02-26 11:57:42 +0800

    对象类型

--- demo3.ts
+++ demo3.ts
@@ -1,4 +1,24 @@
-interface Lao {
+const Lao : {
   uname: string,
   age: number
+} = {
+  uname: '写到脚',
+  age: 19
 }
+
+// console.log(Lao.uname)
+
+const xiaoJieJies: String[] = ["谢大脚", "刘英", '1223']
+
+class Meinv {
+  name:string
+}
+const dajiao : Meinv = new Meinv()
+console.log(dajiao)
+
+const jianXiaoJieJie: () => string = () => {
+  return "大脚";
+}
+console.log(jianXiaoJieJie())
+
+
