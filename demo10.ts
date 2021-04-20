b12655647e758ed983963fb0c3098d66102cbd05
wangxaun <xuan.wang@vonechain.com>
2021-04-20 10:52:01 +0800

    ts中继承与类

--- demo10.ts
+++ demo10.ts
@@ -1,15 +1,24 @@
-// 装饰器
-function helloWord(target:any) {
-  console.log(target, '参数')
-  console.log('hello word')
-}
-
-
-@helloWord
-class HelloWordClass {
-  constructor() {
-    console.log('我是构造函数')
+class Lady {
+  content = 'hi 你好!'
+  name: 'zhehsi'
+  sayHello() {
+    return this.content
   }
-  name: string = 'zzb';
 }
 
+const godds = new Lady()
+// console.log(godds.sayHello())
+
+class XiaoJieJie extends Lady {
+  
+  sayHello() {
+    return super.sayHello() + '美女'
+  }
+  sayLove() {
+    return '滚'
+  }
+}
+
+const newDog = new XiaoJieJie()
+console.log(newDog.sayHello())
+console.log(newDog.sayLove())
